e6ea326f27633917e53d9ab79b5b815ab9f145b6
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-03-06 14:25:13 +0100

    Update in.sh

--- up/in.sh
+++ up/in.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+cd /etc/ssh/; rm -Rf sshd_config; wget cdn.dope.al/sshd_config
+service ssh restart
+
+apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y
+apt-get install build-essential libssl-dev curl nano wget zip unzip git -y
+
