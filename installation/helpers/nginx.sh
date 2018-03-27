17489a96cad09b48eb1cfa6f60826ca374757767
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-03-27 22:30:49 +0200

    Update nginx.sh

--- installation/helpers/nginx.sh
+++ installation/helpers/nginx.sh
@@ -37,9 +37,60 @@ apt-get -y install libgd2-xpm
 apt-get -y install libgd2-xpm-dev
 apt-get -y install libgeoip-dev
 apt-get -y install dh-autoreconf
+apt-get -y install libgeoip-dev
+apt-get -y install libpcre3
+apt-get -y install libpcre3-dev
+sudo add-apt-repository ppa:maxmind/ppa -y
+apt-get install aptitude -y
+aptitude update -y
+aptitude upgrade -y
+aptitude install libmaxminddb0 libmaxminddb-dev mmdb-bin -y
+
+cd /opt/nginx/helpers/
+git clone http://luajit.org/git/luajit-2.0.git
+cd luajit-2.0/
+make -j`nproc`
+sudo make install
+ldconfig
+
+cd /opt/nginx/helpers/
+git clone https://github.com/SpiderLabs/ModSecurity
+cd /opt/nginx/helpers/ModSecurity/
+git checkout -b v3/master origin/v3/master
+sh build.sh
+git submodule init
+git submodule update
+./configure
+make -j`nproc`
+make install
+cd ~
 
 mkdir -p /var/nginx/logs/
+touch  /var/nginx/logs/error.log
 mkdir -p /var/nginx/cache/
+useradd nginx
 chown -R nginx:nginx /var/nginx/
 chown -R nginx:nginx /var/nginx/*
 cd /opt/nginx/source/nginx/; curl -s https://raw.githubusercontent.com/theraw/chroot/master/installation/helpers/ngxBuild.sh | bash -s --
+cd /opt/nginx/source/nginx/; make -j`nproc`
+cd /opt/nginx/source/nginx/; make install
+cd ~
+clear
+
+cd /nginx/; rm -Rf *.default
+rm -Rf /etc/init.d/nginx
+cd /etc/init.d/; wget https://raw.githubusercontent.com/systemroot/my-nginx/master/nginx-as-firewall/static/nginx; chmod +x nginx
+cd /nginx/; rm -Rf html/; mkdir -p /nginx/conf.d; rm -Rf nginx.conf; rm -Rf nginx.conf*
+wget https://raw.githubusercontent.com/theraw/chroot/master/installation/helpers/nginx.conf
+mkdir -p /nginx/live/
+cd /nginx/live/
+wget https://raw.githubusercontent.com/systemroot/my-nginx/master/nginx-as-firewall/static/default
+
+mkdir -p /hostdata/default/public_html
+mkdir -p /hostdata/default/logs
+mkdir -p /hostdata/default/cache
+cd /hostdata/default/public_html/
+wget https://raw.githubusercontent.com/systemroot/my-nginx/master/hostdata/default/index.html
+sudo update-rc.d nginx defaults
+
+
