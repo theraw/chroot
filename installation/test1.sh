f0f33e8045b8bdf6f6d5f354505b3af30832a3fa
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-03-28 01:56:30 +0200

    Update test1.sh

--- installation/test1.sh
+++ installation/test1.sh
@@ -9,6 +9,13 @@ echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
 apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y
 apt-get -y install build-essential libssl-dev curl nano wget zip unzip git iftop htop
 
+apt install software-properties-common -y
+apt install python-software-properties -y
+sudo add-apt-repository ppa:ondrej/php -y
+apt update; apt upgrade -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
+apt update; apt upgrade -y
+
 clear
 
 curl -s https://raw.githubusercontent.com/theraw/chroot/master/installation/helpers/gcc.sh | bash -s --
